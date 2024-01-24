abe0825e9f47fa7450c635d5b30b449da03f48fb
SabrinaM2 <sabrina.mohammedi@efrei.net>
2024-01-24 01:06:46 +0100

    Initial add of the auth-service (user registration functional)

---
+++ auth-service/todo-auth-web-service/src/main/java/com/example/todoappauthservice/TodoAppAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class TodoAppAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppAuthServiceApplication.class, args);
+	}
+
+}
