4e369f49ede1f2ac03a8a46b1e39f2f29e7af56d
SabrinaM2 <sabrina.mohammedi@efrei.net>
2024-01-26 13:17:30 +0100

    Modification the auth-service (user registration and login functional)

--- auth-service/todo-auth-web-service/src/main/java/com/example/todoappauthservice/constants/UrlConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.todoappauthservice.constants;
-
-public class UrlConstants {
-    public static final String HOME_URL = "/";
-    public static final String REGISTER_URL = "/register";
-    public static final String LOGIN_URL = "/login";
-}
