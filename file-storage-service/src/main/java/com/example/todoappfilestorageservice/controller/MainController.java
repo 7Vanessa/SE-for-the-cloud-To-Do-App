d9e257be2df1c6a01e57295c06eb88f6519639b9
SabrinaM2 <sabrina.mohammedi@efrei.net>
2024-01-30 14:49:21 +0100

    Final settings + add swagger documentation

--- file-storage-service/src/main/java/com/example/todoappfilestorageservice/controller/MainController.java
+++ file-storage-service/src/main/java/com/example/todoappfilestorageservice/controller/MainController.java
@@ -2,33 +2,74 @@ package com.example.todoappfilestorageservice.controller;
 
 import com.example.todoappfilestorageservice.entity.FileEntity;
 import com.example.todoappfilestorageservice.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@Tag(name = "File Storage", description = "Spring Boot API for file storage")
+@RestController
 @RequestMapping(path = "/api/files")
 public class MainController {
     @Autowired
     private FileService fileService;
 
+    @Operation(
+            summary = "Upload a file",
+            description = "Upload a file from your local machine. The response is a String.",
+            tags = { "upload", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = FileService.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @PostMapping(path="/upload")
     public @ResponseBody String uploadReport (@RequestParam("file")MultipartFile file){
         return fileService.upload(file);
     }
 
+    @Operation(
+            summary = "Download a file by ID",
+            description = "Download a file in your local machine by giving its ID. The response is a File.",
+            tags = { "download", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = FileService.class), mediaType = "application/pdf") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/download/{fileId}")
     public ResponseEntity<byte[]> downloadReport(@PathVariable Integer fileId) {
         return fileService.download(fileId);
     }
 
+    @Operation(
+            summary = "Display all the uploaded files",
+            description = "Display all the uploaded previous files. The response is a list of Files.",
+            tags = { "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/all")
     public @ResponseBody Iterable<FileEntity> getAllFiles() {
         return fileService.getAll();
     }
 
+    @Operation(
+            summary = "Delete a file by ID",
+            description = "Delete a file in the list of uploaded files by giving its ID.",
+            tags = { "delete" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @DeleteMapping("/{fileId}")
     public ResponseEntity<String> deleteFile(@PathVariable Integer fileId) {
         fileService.deleteFile(fileId);
--- taskmanagerservice/src/main/java/com/example/taskmanagerservice/controller/TaskController.java
+++ taskmanagerservice/src/main/java/com/example/taskmanagerservice/controller/TaskController.java
@@ -3,6 +3,12 @@ package com.example.taskmanagerservice.controller;
 import com.example.taskmanagerservice.dto.TaskDTO;
 import com.example.taskmanagerservice.entity.Task;
 import com.example.taskmanagerservice.service.TaskManagerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Tag(name = "Task Manager", description = "Spring Boot API for managing tasks")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
@@ -18,6 +25,15 @@ public class TaskController {
     @Autowired
     private TaskManagerService taskManagerService;
 
+    @Operation(
+            summary = "Create a task",
+            description = "Create a new task by providing the task title and its description. The response is a Task.",
+            tags = { "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TaskManagerService.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @PostMapping
     public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
         // Convert TaskDTO to Task entity and save to the database
@@ -28,6 +44,15 @@ public class TaskController {
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Create a task",
+            description = "Create a new task by providing the task title and its description. The response is a Task.",
+            tags = { "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TaskManagerService.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/{taskId}")
     public ResponseEntity<TaskDTO> getTask(@PathVariable Long taskId) {
         // Retrieve Task from the database by ID
@@ -38,6 +63,15 @@ public class TaskController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Display a list of tasks",
+            description = "Display the list of all the added tasks. The response is a list of Task.",
+            tags = { "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TaskManagerService.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping
     public ResponseEntity<List<TaskDTO>> getAllTasks() {
         // Retrieve all tasks from the database
@@ -51,6 +85,15 @@ public class TaskController {
         return new ResponseEntity<>(responseDTOs, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update the status of a task",
+            description = "Update the status of a task (completed or not) by giving its id. The response is a Task.",
+            tags = { "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TaskManagerService.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @PutMapping("/{taskId}")
     public ResponseEntity<TaskDTO> updateTask(@PathVariable Long taskId, @RequestBody TaskDTO taskDTO) {
         // Update Task in the database
@@ -61,6 +104,15 @@ public class TaskController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete a task by ID",
+            description = "Delete a task from the list of tasks by giving its ID.",
+            tags = { "delete" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = TaskManagerService.class)) }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @DeleteMapping("/{taskId}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
         // Delete Task from the database
